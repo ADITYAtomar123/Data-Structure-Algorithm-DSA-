2d5c15daaa116b6e8f1ebd8ee84dcfcaf4fdb681
Aditya Tomar <adityatomar835@gmail.com>
2026-01-28 23:29:38 +0530

    Add files via upload

---
+++ BuiltInMethods.java
@@ -0,0 +1,35 @@
+package String;
+// public class BuiltInMethods {
+//     public static void main(String[] args) {
+//         String s="aditya tomar";
+//         System.out.println(s.indexOf('a'));
+//         System.out.println(s.lastIndexOf('a'));
+//         System.out.println(s.indexOf('w'));
+//     }
+// }
+
+
+
+// public class BuiltInMethods {
+//     public static void main(String[] args) {
+//         String s="Aditya Tomar";
+//         System.out.println(s.toLowerCase());
+//         System.out.println(s.toUpperCase());
+//     }
+// }
+
+
+
+
+// public class BuiltInMethods {
+//     public static void main(String[] args) {
+//         String s="Aditya Tomar";
+//         System.out.println(s.contains("tya"));
+//         System.out.println(s.contains("om"));
+//         System.out.println(s.contains("tr"));
+//     }
+// }
+
+
+
+
---
+++ CampareToStringMethod.java
@@ -0,0 +1,11 @@
+package String;
+public class CampareToStringMethod {
+    public static void main(String[] args) {
+        String a = "harmeet";
+        String b = "harsh";
+        System.out.println(a.compareTo(b));
+        String c = "harsh";
+        String d = "harsh";
+        System.out.println(c.compareTo(d));
+    }
+}
