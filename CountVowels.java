216ca6745dec4c4607c2631d5eb89159916bd047
Aditya Tomar <adityatomar835@gmail.com>
2026-01-28 23:33:59 +0530

    Add files via upload

---
+++ CountVowels.java
@@ -0,0 +1,17 @@
+package String;
+// count the vowels in a given string
+import java.util.*;
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int count =0;
+        for(int i =0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
