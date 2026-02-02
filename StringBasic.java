9beb0c6965a41d8f241ef8bf0cf1c439299bebfe
Aditya Tomar <adityatomar835@gmail.com>
2026-02-02 19:43:08 +0530

    Add files via upload

---
+++ StringBasic.java
@@ -0,0 +1,33 @@
+// package String;
+// import java.util.*;
+// public class StringBasic {
+//     public static void main(String[] args) {
+//         String s = "aditya tomar";
+//         System.out.println(s);
+//     }
+// }
+
+
+
+// package String;
+// import java.util.*;
+// public class StringBasic {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         // String s = sc.next();
+//         // System.out.println(s);
+//         String s1 = sc.nextLine();
+//         System.out.println(s1);
+//     }
+// }
+
+
+
+package String;
+import java.util.*;
+public class StringBasic {
+    public static void main(String[] args) {
+        String s = "aditya";
+        System.out.println(s.charAt(2));
+    }
+}
